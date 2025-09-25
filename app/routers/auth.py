cb8f2e472940325a1904820293e9fde7649a39b6
Ashini Singha <ashini.singha@enosisbd.com>
2025-09-26 02:48:32 +0600

    feat(login) : add user.id as response in /login endpoint for checking user.id with places owner id

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -39,4 +39,4 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", "user_id": user.id}
